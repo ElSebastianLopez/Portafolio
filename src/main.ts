6c52d767f6c5f1cb1b26c2d65a9c87e3084a7d6a
ElSebastianLopez <sebaslopezhenao@gmail.com>
2022-08-01 17:47:52 -0500

    continue with the structure of my portfolio

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,35 @@
 import { createApp } from 'vue';
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+import {
+  faDatabase,
+} from '@fortawesome/free-solid-svg-icons';
+
+/* import specific icons */
+import {
+  faHtml5,
+  faSquareJs,
+  faVuejs,
+  faCss3Alt,
+  faJava,
+
+} from '@fortawesome/free-brands-svg-icons';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+/* add icons to the library */
+library.add(
+  faVuejs,
+  faSquareJs,
+  faHtml5,
+  faCss3Alt,
+  faJava,
+  faDatabase,
+);
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(store).use(router)
+  .mount('#app');
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,14 +3,13 @@
     <div class="containerFrist">
       <div class="containerCenter">
         <div class="img">
-          <img src="../assets/logo.png" alt="">
+          <img src="../assets/logo.png" alt="" />
         </div>
         <div>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut atque totam impedit, </p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut atque totam impedit,</p>
         </div>
       </div>
-      <div>
-      </div>
+      <div></div>
     </div>
     <div>
       <h2>PROYECTOS</h2>
@@ -18,11 +17,44 @@
     <div class="secondContainer">
       <div class="sectionOne">
         <div class="picture">img</div>
-        <div class="text">tex</div>
+        <div class="text">
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita sed adipisci, dolore
+            amet aspernatur labore enim ab tempore, quis ducimus itaque! Veniam, veritatis. Eius
+            autem consequuntur vel ratione reprehenderit atque?
+          </p>
+        </div>
       </div>
       <div class="sectionTwo">
-        <div class="picture2">im</div>
-        <div class="text2">tex</div>
+        <div class="picture">im</div>
+        <div class="text">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita sed adipisci, dolore
+            amet aspernatur labore enim ab tempore, quis ducimus itaque! Veniam, veritatis. Eius
+            autem consequuntur vel ratione reprehenderit atque?
+          </p>
+        </div>
+      </div>
+    </div>
+    <div></div>
+    <div class="containerSkills">
+      <div class="title">
+        <h2 class="title-h2">SKILLS</h2>
+      </div>
+      <div class="skills">
+        <font-awesome-icon icon="fa-brands fa-vuejs" />
+
+        <font-awesome-icon icon="fa-brands fa-square-js" />
+
+        <font-awesome-icon icon="fa-brands fa-html5" />
+
+        <font-awesome-icon icon="fa-brands fa-css3-alt" />
+
+        <font-awesome-icon icon="fa-brands fa-java" />
+
+        <font-awesome-icon icon="fa-solid fa-database" />
+
+        <h1 class="c">C#</h1>
       </div>
     </div>
     <div>
@@ -32,30 +64,30 @@
       <h2>EXPERIENCIA</h2>
     </div>
     <footer>
-    <div>
-      <h2>CONTACTAME</h2>
-    </div>
+      <div>
+        <h2>CONTACTAME</h2>
+      </div>
     </footer>
   </div>
 </template>
 
-<script lang="ts">
-</script>
+<script lang="ts"></script>
 
 <style lang="scss">
-@import '../assets/styles/main.scss';
+@import "../assets/styles/main.scss";
 
-.img img{
+.img img {
   width: 50px;
   height: 50px;
   padding-left: 48px;
-  padding-top:10px ;
+  padding-top: 10px;
 }
- @media (min-width: 820px) {
-  .img img{
-  width: 70px;
-  height: 80px;
-  padding-right: 40px;
+
+@media (min-width: 820px) {
+  .img img {
+    width: 70px;
+    height: 80px;
+    padding-right: 40px;
+  }
 }
- }
 </style>
